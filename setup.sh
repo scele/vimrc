a3f5f53c62735861c186bf342d81dd76e265ef2a
Lauri Peltonen <lauri.peltonen@iki.fi>
2015-01-01 22:28:09 +0000

    Add gitconfig, init submodules in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git submodule init
+git submodule update
 ln -s ../bundle/pathogen/autoload/pathogen.vim autoload/pathogen.vim
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s .vim/vimrc ~/.vimrc
+ln -s .vim/gvimrc ~/.gvimrc
+ln -s .vim/gitconfig ~/.gitconfig
